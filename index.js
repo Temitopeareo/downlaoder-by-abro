1e0cd70468299298ea7e07783e900fa942eb564c
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2024-09-25 15:31:24 +0100

    NPM

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const YouTubeHelper = require('./youtube-utils');
-const ytHelper = new YouTubeHelper('AIzaSyCQIAvY9aNozPzjYQBJepDVcDA9ktNPQ_I')
-
-
-This is the Api I got used credentials);
-// Get download URLs
-ytHelper.getDownloadUrls('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-  .then(urls => console.log(urls))
-  .catch(err => console.error(err));
 
 
 module.exports = YouTubeHelper;
